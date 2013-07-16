78a468aeabe37e97cc07507afa49d2d307f43aa1
César Luis Alvargonzález <sisa989@gmail.com>
2013-07-16 21:33:18 +0200

    Añadido Chomeless Editor con soporte full screen, blobs, etc

--- FILE API & DnD/js/site.js
+++ FILE API & DnD/js/site.js
@@ -1,49 +1,74 @@
 $().ready(function() {
 
-	var box = document.getElementById('editor');
-	
-	box.addEventListener('dragover', cancel);
-	
-	box.addEventListener('dragenter', cancel);
-	
-	box.addEventListener('drop', function(event) {
-	
+	var editor = document.getElementById('editor');
+
+	$("#download").click(function() {
+		var blob = new Blob([$(editor).text()], {
+			type : "text"
+		});
+		window.open(window.URL.createObjectURL(blob), "Chromeless - Editor: New Saved File");
+	});
+
+	$("#trash").click(function() {
+		$("#editor").empty();
+	});
+
+	$("#left").click(function() {
+		$("#editor").css("text-align", "left");
+	});
+
+	$("#center").click(function() {
+		$("#editor").css("text-align", "center");
+	});
+
+	$("#right").click(function() {
+		$("#editor").css("text-align", "right");
+	});
+
+	$("#justify").click(function() {
+		$("#editor").css("text-align", "justify");
+	});
+
+	editor.addEventListener('dragover', cancel);
+
+	editor.addEventListener('dragenter', cancel);
+
+	editor.addEventListener('drop', function(event) {
+
 		if (event.preventDefault) {
 			event.preventDefault();
 		}
-		
+
 		files = event.dataTransfer.files;
-		
-		for(i = 0; i < files.length; i++){
+
+		for ( i = 0; i < files.length; i++) {
 			cargarImagen(files[i]);
 		}
-		
+
 		return false;
 	});
-	
+
 	function cancel(event) {
 		if (event.preventDefault) {
 			event.preventDefault();
 		}
 		return false;
 	}
-	
-	function cargarImagen(file){
-		// display text
+
+	function cargarImagen(file) {
 		if (file.type.indexOf("text") == 0) {
 			var reader = new FileReader();
 			reader.onload = function(e) {
-				añadirHtml(
-					e.target.result.replace(/</g, "&lt;").replace(/>/g, "&gt;")
-				);
+				añadirHtml(e.target.result.replace(/</g, "&lt;").replace(/>/g, "&gt;"));
 			}
 			reader.readAsText(file);
 		}
 	}
-	
+
 	function añadirHtml(html) {
 		$('#editor').html(function(index, val) {
 			return $('#result').html() + html;
 		});
 	}
-});
+
+}); 
